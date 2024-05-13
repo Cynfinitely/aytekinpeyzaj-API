10e9f04fc1dacd449ccc78c824c7d59facb1ea62
Celal Yasin Nari <celalyasinnari@gmail.com>
2024-05-13 23:39:10 +0300

    spring initiate

---
+++ src/main/java/com/aytekinpeyzaj/aytekinpeyzaj/AytekinpeyzajApplication.java
@@ -0,0 +1,13 @@
+package com.aytekinpeyzaj.aytekinpeyzaj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AytekinpeyzajApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AytekinpeyzajApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aytekinpeyzaj/aytekinpeyzaj/AytekinpeyzajApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aytekinpeyzaj.aytekinpeyzaj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AytekinpeyzajApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
